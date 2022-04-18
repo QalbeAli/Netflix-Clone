165f81145b47dba1d9c445ad8ddeb13e29a96d21
QalbeAli <qalbeali8@gmail.com>
2022-04-18 17:35:38 +0500

    remove scroll bar

--- components/Row.tsx
+++ components/Row.tsx
@@ -12,7 +12,7 @@ export default function Row({ title, movies }: Props) {
       <h2 className="width-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">{title}</h2>
       <div className="md: group relative -ml-2">
         <ChevronLeftIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 hover:scale-125 group-hover:opacity-100" />
-        <div className="flex items-center overflow-x-scroll sapce-x-0.5  md:space-x-2.5">
+        <div className="flex items-center scrollbar-hide overflow-x-scroll overflow-y-hidden sapce-x-0.5  md:space-x-2.5">
             {movies.map((movie) => (
                 <Thumbnail key={movie.id} movie={movie} />
             ))}
