dd836282617c2014d20ab8fdb3d54a177ebed147
QalbeAli <qalbeali8@gmail.com>
2022-04-21 11:07:19 +0500

    subscription

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -22,7 +22,6 @@ function Login() {
   } = useForm<Inputs>()
 
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    console.log(data)
     if (login) {
       await signIn(data.email, data.password)
     } else {
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import useAuth from '../hooks/UseAuth'
 import {modalState} from "../atoms/modalAtom"
 import Modal from '../components/Modal'
 import { useRecoilValue } from 'recoil'
+import Plans from '../components/Plans'
 
 interface Props {
   netflixOriginals: Movie[]
@@ -32,6 +33,11 @@ const Home = ({
 }: Props) => {
   const {loading} = useAuth()
   const showModal = useRecoilValue(modalState)
+  const subscription = false
+
+  if(loading || subscription === null) return null
+
+  // if (!subscription) return <Plans />
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ">
       <Head>
